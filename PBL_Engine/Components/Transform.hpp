c73bc7a5d9b24b47a40813eaeac7a786eb7fcef9
Tomasz Witczak <TomaszMWitczak@gmail.com>
2020-04-15 08:02:24 +0200

    Add first integration of ECS and scene loading
    * Move GameObject data to Transform component
    * Add convenience macro for iterating loops with yaml-cpp
    * Divide loading into sections: firstly create entities,
       then load components
    * For Transform component, update parent references
    * Store scene nodes in a set to make further traversal easier

--- PBL_Engine/Components/Transform.hpp
+++ PBL_Engine/Components/Transform.hpp
@@ -4,6 +4,25 @@
 #include "ECS/Component.hpp"
 
 // //////////////////////////////////////////////////////////////// Component //
-ECS_COMPONENT(Transform){};
+ECS_COMPONENT(Transform) {
+    Transform *parent;
+
+    // TODO: Replace these with appropriate vectors
+    float rotation_x;
+    float rotation_y;
+    float rotation_z;
+    float rotation_w;
+    float position_x;
+    float position_y;
+    float position_z;
+    float scale_x;
+    float scale_y;
+    float scale_z;
+    float eulerAngle_x;
+    float eulerAngle_y;
+    float eulerAngle_z;
+
+    int root_Order;
+};
 
 // ////////////////////////////////////////////////////////////////////////// //
--- PBL_Engine/LevelParser.cpp
+++ PBL_Engine/LevelParser.cpp
@@ -1,130 +1,164 @@
 #include "LevelParser.h"
 
+#include <cassert>
+#include <set>
 #include <unordered_map>
 #include <vector>
 
+#include "Components/Components.hpp"
+#include "ECS/ECS.hpp"
 #include "yaml-cpp/include/yaml-cpp/yaml.h"
 
-struct GameObject {
-    std::string nazwa;
-    int rotation_x;
-    int rotation_y;
-    int rotation_z;
-    int rotation_w;
-    int position_x;
-    int position_y;
-    int position_z;
-    int scale_x;
-    int scale_y;
-    int scale_z;
-    int root_Order;
-    int eulerAngle_x;
-    int eulerAngle_y;
-    int eulerAngle_z;
-};
+auto &registry = Registry::instance();
+
+#define yamlLoop(iterator, node)                         \
+    for (YAML::const_iterator iterator = (node).begin(); \
+         iterator != (node).end(); ++iterator)
 
 LevelParser::LevelParser() {}
 
 LevelParser::~LevelParser() {}
 
 void LevelParser::load() {
-    std::vector<YAML::Node> nodes = YAML::LoadAllFromFile("1.yaml");
+    using FileId = std::string;
 
-    // Map all possible nodes with corresponding identifiers
-    std::unordered_map<std::string, YAML::Node> map;
-    for (auto const &document : nodes) {
-        for (YAML::const_iterator i = document.begin(); i != document.end();
-             ++i) {
+    auto const &sceneNodes = YAML::LoadAllFromFile("1.yaml");
+
+    // Map all possible nodes with corresponding identifiers and make a set of
+    // all identifiers in scene file
+    std::unordered_map<FileId, YAML::Node> nodes;
+    std::set<FileId> sceneFileIds;
+    for (auto const &node : sceneNodes) {
+        yamlLoop(i, node) {
             if (!i->second["id"]) {
                 continue;
             }
-            map.insert({i->second["id"].as<std::string>(), document});
+            nodes.insert({i->second["id"].as<FileId>(), node});
+            sceneFileIds.insert(i->second["id"].as<FileId>());
         }
     }
 
-    GameObject level[100];
-    int j = 0;
-    for (int i = 0; i < nodes.size(); i++) {
-        if (nodes[i]["GameObject"]) {
-            if (nodes[i]["GameObject"]["m_Name"]) {
-                level[j].nazwa =
-                    nodes[i]["GameObject"]["m_Name"].as<std::string>();
+    // Go through all game objects in scene file and create entities
+    std::unordered_map<FileId, EntityId> entities;
+    for (auto const &fileId : sceneFileIds) {
+        auto node{nodes[fileId]};
+
+        if (node["GameObject"]) {
+            auto entity{registry.createEntity()};
+            entities.insert({fileId, entity.id});
+        }
+    }
+
+    // Go through all other components in the scene file
+    // For now, only process Transform
+    // TODO: Expand this only when new components are needed
+    std::unordered_map<FileId, EntityId> entityIdsWithTransform;
+    for (auto const &fileId : sceneFileIds) {
+        auto const &node = nodes[fileId];
+
+        if (auto const &nodeTransform = node["Transform"]; nodeTransform) {
+            assert(nodeTransform["m_GameObject"] &&
+                   nodeTransform["m_LocalRotation"] &&
+                   nodeTransform["m_LocalPosition"] &&
+                   nodeTransform["m_LocalScale"] &&
+                   nodeTransform["m_RootOrder"] &&
+                   nodeTransform["m_LocalEulerAnglesHint"] &&
+                   "Every property inside Transform component must be valid!");
+
+            yamlLoop(i, nodeTransform["m_GameObject"]) {
+                auto gameObjectFileId = i->second.Scalar();
+                Entity(entities[gameObjectFileId]).add<Transform>({});
+                entityIdsWithTransform.insert(
+                    {fileId, entities[gameObjectFileId]});
+            }
+
+            auto &transform =
+                Entity(entityIdsWithTransform[fileId]).get<Transform>();
+
+            yamlLoop(it, nodeTransform["m_LocalRotation"]) {
+                auto const key = it->first.as<std::string>();
+                auto const value = it->second.as<float>();
+
+                if (key == "x") {
+                    transform.rotation_x = value;
+                }
+                if (key == "y") {
+                    transform.rotation_y = value;
+                }
+                if (key == "z") {
+                    transform.rotation_z = value;
+                }
+                if (key == "w") {
+                    transform.rotation_w = value;
+                }
+            }
+
+            yamlLoop(it, nodeTransform["m_LocalPosition"]) {
+                auto const key = it->first.as<std::string>();
+                auto const value = it->second.as<float>();
+
+                if (key == "x") {
+                    transform.position_x = value;
+                }
+                if (key == "y") {
+                    transform.position_y = value;
+                }
+                if (key == "z") {
+                    transform.position_z = value;
+                }
+            }
+
+            yamlLoop(it, nodeTransform["m_LocalScale"]) {
+                auto const key = it->first.as<std::string>();
+                auto const value = it->second.as<float>();
+
+                if (key == "x") {
+                    transform.scale_x = value;
+                }
+                if (key == "y") {
+                    transform.scale_y = value;
+                }
+                if (key == "z") {
+                    transform.scale_z = value;
+                }
+            }
+
+            transform.root_Order = nodeTransform["m_RootOrder"].as<int>();
+
+            yamlLoop(it, nodeTransform["m_LocalEulerAnglesHint"]) {
+                auto const key = it->first.as<std::string>();
+                auto const value = it->second.as<float>();
+
+                if (key == "x") {
+                    transform.eulerAngle_x = value;
+                }
+                if (key == "y") {
+                    transform.eulerAngle_y = value;
+                }
+                if (key == "z") {
+                    transform.eulerAngle_z = value;
+                }
             }
         }
-        if (nodes[i]["Transform"]) {
-            if (nodes[i]["Transform"]["m_LocalRotation"]) {
-                for (YAML::const_iterator it =
-                         nodes[i]["Transform"]["m_LocalRotation"].begin();
-                     it != nodes[i]["Transform"]["m_LocalRotation"].end();
-                     it++) {
-                    if (it->first.as<std::string>() == "x") {
-                        level[j].rotation_x = it->second.as<float>();
-                    }
-                    if (it->first.as<std::string>() == "y") {
-                        level[j].rotation_y = it->second.as<float>();
-                    }
-                    if (it->first.as<std::string>() == "z") {
-                        level[j].rotation_z = it->second.as<float>();
-                    }
-                    if (it->first.as<std::string>() == "w") {
-                        level[j].rotation_w = it->second.as<float>();
-                    }
-                }
-            }
-            if (nodes[i]["Transform"]["m_LocalPosition"]) {
-                for (YAML::const_iterator it =
-                         nodes[i]["Transform"]["m_LocalPosition"].begin();
-                     it != nodes[i]["Transform"]["m_LocalPosition"].end();
-                     it++) {
-                    if (it->first.as<std::string>() == "x") {
-                        level[j].position_x = it->second.as<float>();
-                    }
-                    if (it->first.as<std::string>() == "y") {
-                        level[j].position_y = it->second.as<float>();
-                    }
-                    if (it->first.as<std::string>() == "z") {
-                        level[j].position_z = it->second.as<float>();
-                    }
-                }
-            }
-            if (nodes[i]["Transform"]["m_LocalScale"]) {
-                for (YAML::const_iterator it =
-                         nodes[i]["Transform"]["m_LocalScale"].begin();
-                     it != nodes[i]["Transform"]["m_LocalScale"].end(); it++) {
-                    if (it->first.as<std::string>() == "x") {
-                        level[j].scale_x = it->second.as<float>();
-                    }
-                    if (it->first.as<std::string>() == "y") {
-                        level[j].scale_y = it->second.as<float>();
-                    }
-                    if (it->first.as<std::string>() == "z") {
-                        level[j].scale_z = it->second.as<float>();
-                    }
-                }
-            }
-            if (nodes[i]["Transform"]["m_RootOrder"]) {
-                level[j].root_Order =
-                    nodes[i]["Transform"]["m_RootOrder"].as<int>();
-            }
-            if (nodes[i]["Transform"]["m_LocalEulerAnglesHint"]) {
-                for (YAML::const_iterator it =
-                         nodes[i]["Transform"]["m_LocalEulerAnglesHint"]
-                             .begin();
-                     it !=
-                     nodes[i]["Transform"]["m_LocalEulerAnglesHint"].end();
-                     it++) {
-                    if (it->first.as<std::string>() == "x") {
-                        level[j].eulerAngle_x = it->second.as<float>();
-                    }
-                    if (it->first.as<std::string>() == "y") {
-                        level[j].eulerAngle_y = it->second.as<float>();
-                    }
-                    if (it->first.as<std::string>() == "z") {
-                        level[j].eulerAngle_z = it->second.as<float>();
-                    }
-                }
-            }
+    }
+
+    // After creating Transform components, update parent references
+    // TODO: Consider also adding child references, somehow
+    for (auto const &[fileId, entityId] : entityIdsWithTransform) {
+        auto const &node{nodes[fileId]};
+        assert(node["Transform"] &&
+               "This loop must operate on valid Transform components!");
+        assert(node["Transform"]["m_Father"] &&
+               node["Transform"]["m_Father"]["fileID"] &&
+               "Transform components in game files must contain parent "
+               "reference!");
+
+        auto const &parentFileId =
+            node["Transform"]["m_Father"]["fileID"].Scalar();
+        if (!entityIdsWithTransform.contains(parentFileId)) {
+            continue;
         }
-        j++;
+        Entity{entityId}.get<Transform>().parent =
+            &Entity{entityIdsWithTransform[parentFileId]}.get<Transform>();
     }
 }
