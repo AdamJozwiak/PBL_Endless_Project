24cabb55aa02494f34dd8a8ca17a57f4246af560
umcikikumba <pileccki@gmailc.om>
2020-04-18 00:20:35 +0200

    Implement basic level parsing operations

---
+++ PBL_Engine/LevelParser.cpp
@@ -0,0 +1,117 @@
+#include "LevelParser.h"
+
+#include <vector>
+
+#include "yaml-cpp/include/yaml-cpp/yaml.h"
+
+struct GameObject {
+    std::string nazwa;
+    int rotation_x;
+    int rotation_y;
+    int rotation_z;
+    int rotation_w;
+    int position_x;
+    int position_y;
+    int position_z;
+    int scale_x;
+    int scale_y;
+    int scale_z;
+    int root_Order;
+    int eulerAngle_x;
+    int eulerAngle_y;
+    int eulerAngle_z;
+};
+
+LevelParser::LevelParser() {}
+
+LevelParser::~LevelParser() {}
+
+void LevelParser::load() {
+    std::vector<YAML::Node> nodes = YAML::LoadAllFromFile("1.unity");
+
+    GameObject level[10];
+    int j = 0;
+    for (int i = 0; i < nodes.size(); i++) {
+        if (nodes[i]["GameObject"]) {
+            if (nodes[i]["GameObject"]["m_Name"]) {
+                level[j].nazwa =
+                    nodes[i]["GameObject"]["m_Nazwa"].as<std::string>();
+            }
+        }
+        if (nodes[i]["Transform"]) {
+            if (nodes[i]["Transform"]["m_LocalRotation"]) {
+                for (YAML::const_iterator it =
+                         nodes[i]["Transform"]["m_LocalRotation"].begin();
+                     it != nodes[i]["Transform"]["m_LocalRotation"].end();
+                     it++) {
+                    if (it->first.as<std::string>() == "x") {
+                        level[j].rotation_x = it->second.as<float>();
+                    }
+                    if (it->first.as<std::string>() == "y") {
+                        level[j].rotation_y = it->second.as<float>();
+                    }
+                    if (it->first.as<std::string>() == "z") {
+                        level[j].rotation_z = it->second.as<float>();
+                    }
+                    if (it->first.as<std::string>() == "w") {
+                        level[j].rotation_w = it->second.as<float>();
+                    }
+                }
+            }
+            if (nodes[i]["Transform"]["m_LocalPosition"]) {
+                for (YAML::const_iterator it =
+                         nodes[i]["Transform"]["m_LocalPosition"].begin();
+                     it != nodes[i]["Transform"]["m_LocalPosition"].end();
+                     it++) {
+                    if (it->first.as<std::string>() == "x") {
+                        level[j].position_x = it->second.as<float>();
+                    }
+                    if (it->first.as<std::string>() == "y") {
+                        level[j].position_y = it->second.as<float>();
+                    }
+                    if (it->first.as<std::string>() == "z") {
+                        level[j].position_z = it->second.as<float>();
+                    }
+                }
+            }
+            if (nodes[i]["Transform"]["m_LocalScale"]) {
+                for (YAML::const_iterator it =
+                         nodes[i]["Transform"]["m_LocalScale"].begin();
+                     it != nodes[i]["Transform"]["m_LocalScale"].end(); it++) {
+                    if (it->first.as<std::string>() == "x") {
+                        level[j].scale_x = it->second.as<float>();
+                    }
+                    if (it->first.as<std::string>() == "y") {
+                        level[j].scale_y = it->second.as<float>();
+                    }
+                    if (it->first.as<std::string>() == "z") {
+                        level[j].scale_z = it->second.as<float>();
+                    }
+                }
+            }
+            if (nodes[i]["Transform"]["m_RootOrder"]) {
+                level[j].root_Order =
+                    nodes[i]["Transform"]["m_RootOrder"].as<int>();
+            }
+            if (nodes[i]["Transform"]["m_LocalEulerAnglesHint"]) {
+                for (YAML::const_iterator it =
+                         nodes[i]["Transform"]["m_LocalEulerAnglesHint"]
+                             .begin();
+                     it !=
+                     nodes[i]["Transform"]["m_LocalEulerAnglesHint"].end();
+                     it++) {
+                    if (it->first.as<std::string>() == "x") {
+                        level[j].eulerAngle_x = it->second.as<float>();
+                    }
+                    if (it->first.as<std::string>() == "y") {
+                        level[j].eulerAngle_y = it->second.as<float>();
+                    }
+                    if (it->first.as<std::string>() == "z") {
+                        level[j].eulerAngle_z = it->second.as<float>();
+                    }
+                }
+            }
+        }
+        j++;
+    }
+}
