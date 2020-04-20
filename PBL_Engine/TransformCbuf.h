5d634280799c6719f3d0aac7359cb7dafd0cf920
Maciej <darkuso@o2.pl>
2020-04-20 20:37:18 +0200

    Pack matrices into Transforms structure

--- PBL_Engine/TransformCbuf.h
+++ PBL_Engine/TransformCbuf.h
@@ -1,14 +1,22 @@
 #pragma once
-#include "ConstantBuffers.h"
-#include "Renderable.h"
 #include <DirectXMath.h>
 
-class TransformCbuf : public Bindable
-{
-public:
-	TransformCbuf(Graphics& gfx, const Renderable& parent);
-	void Bind(Graphics& gfx) noexcept override;
-private:
-	static std::unique_ptr<VertexConstantBuffer<DirectX::XMMATRIX>> pVcbuf; // dynamically allocated pointer to ConstantBuffer
-	const Renderable& parent;
+#include "ConstantBuffers.h"
+#include "Renderable.h"
+
+class TransformCbuf : public Bindable {
+  private:
+    struct Transforms {
+        DirectX::XMMATRIX modelViewProj;
+        DirectX::XMMATRIX model;
+    };
+
+  public:
+    TransformCbuf(Graphics& gfx, const Renderable& parent, UINT slot = 0u);
+    void Bind(Graphics& gfx) noexcept override;
+
+  private:
+    static std::unique_ptr<VertexConstantBuffer<Transforms>>
+        pVcbuf;  // dynamically allocated pointer to ConstantBuffer
+    const Renderable& parent;
 };
