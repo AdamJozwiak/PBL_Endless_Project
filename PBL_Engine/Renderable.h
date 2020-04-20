19bc5c072bb29996049696eefeb3190cd8578163
Maciej <darkuso@o2.pl>
2020-04-20 20:27:31 +0200

    Add QueryBindable template function

--- PBL_Engine/Renderable.h
+++ PBL_Engine/Renderable.h
@@ -1,26 +1,41 @@
 #pragma once
-#include "Graphics.h"
 #include <DirectXMath.h>
+
+#include "Graphics.h"
 
 class Bindable;
 
-class Renderable
-{
-	template<class T>
-	friend class RenderableBase;
-public:
-	Renderable() = default;
-	Renderable(const Renderable&) = delete;
-	virtual DirectX::XMMATRIX GetTransformXM() const noexcept = 0;
-	void Draw(Graphics& gfx) const noexcept(!IS_DEBUG);
+class Renderable {
+    template <class T>
+    friend class RenderableBase;
+
+  public:
+    Renderable() = default;
+    Renderable(const Renderable&) = delete;
+    virtual DirectX::XMMATRIX GetTransformXM() const noexcept = 0;
+    void Draw(Graphics& gfx) const noexcept(!IS_DEBUG);
 	virtual void Update(float dt) noexcept = 0;
-	virtual ~Renderable() = default;
-protected:
-	void AddBind(std::unique_ptr<Bindable> bind) noexcept(!IS_DEBUG);
-	void AddIndexBuffer(std::unique_ptr<class IndexBuffer> ibuf) noexcept(!IS_DEBUG);
-private:
-	virtual const std::vector<std::unique_ptr<Bindable>>& GetStaticBinds() const noexcept = 0;
-private:
-	const IndexBuffer* pIndexBuffer = nullptr;
-	std::vector<std::unique_ptr<Bindable>> binds;
+    virtual ~Renderable() = default;
+
+  protected:
+    template <class T>
+    T* QueryBindable() noexcept {
+        for (auto& pb : binds) {
+            if (auto pt = dynamic_cast<T*>(pb.get())) {
+                return pt;
+            }
+        }
+        return nullptr;
+    }
+    void AddBind(std::unique_ptr<Bindable> bind) noexcept(!IS_DEBUG);
+    void AddIndexBuffer(std::unique_ptr<class IndexBuffer> ibuf) noexcept(
+        !IS_DEBUG);
+
+  private:
+    virtual const std::vector<std::unique_ptr<Bindable>>& GetStaticBinds()
+        const noexcept = 0;
+
+  private:
+    const IndexBuffer* pIndexBuffer = nullptr;
+    std::vector<std::unique_ptr<Bindable>> binds;
 };
