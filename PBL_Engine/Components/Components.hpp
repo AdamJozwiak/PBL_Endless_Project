bd0a60c6d4945f4349450bf04a1b84bc08cc6cf8
Tomasz Witczak <TomaszMWitczak@gmail.com>
2020-04-22 11:09:10 +0200

    Add MeshFilter component

--- PBL_Engine/Components/Components.hpp
+++ PBL_Engine/Components/Components.hpp
@@ -3,6 +3,7 @@
 // ////////////////////////////////////////////// Includes for all components //
 #include "Behaviour.hpp"
 #include "BoxCollider.h"
+#include "MeshFilter.hpp"
 #include "Renderer.hpp"
 #include "SphereCollider.h"
 #include "Transform.hpp"
