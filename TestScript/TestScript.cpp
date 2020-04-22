70270e7e6ec4e9e01051b8d15c03a2f873f21799
Tomasz Witczak <TomaszMWitczak@gmail.com>
2020-04-22 10:31:36 +0200

    Add simple script for object movement

--- TestScript/TestScript.cpp
+++ TestScript/TestScript.cpp
@@ -4,6 +4,7 @@
 #include "Components/Components.hpp"
 #include "ECS/ECS.hpp"
 #include "Systems/Systems.hpp"
+#include "Window.h"
 
 // ///////////////////////////////////////////////////////// Factory function //
 extern "C" TESTSCRIPT_API void create(std::shared_ptr<Script>& script,
@@ -16,9 +17,30 @@ extern "C" TESTSCRIPT_API void create(std::shared_ptr<Script>& script,
 TestScript::TestScript(Entity const& entity) : Script(entity){};
 
 // ----------------------------------------- System's virtual functions -- == //
-void TestScript::setup(){};
+void TestScript::setup() {
+    isKeyPressed = [](int const key) {
+        return registry.system<RenderSystem>()->window->keyboard.KeyIsPressed(
+            key);
+    };
+};
 
-void TestScript::update(float const deltaTime){};
+void TestScript::update(float const deltaTime) {
+    auto& transform = entity.get<Transform>();
+    transform.eulerAngle_y += 45.0f * deltaTime;
+
+    if (isKeyPressed('A')) {
+        transform.position_x -= 5.0f * deltaTime;
+    }
+    if (isKeyPressed('D')) {
+        transform.position_x += 5.0f * deltaTime;
+    }
+    if (isKeyPressed('W')) {
+        transform.position_z += 5.0f * deltaTime;
+    }
+    if (isKeyPressed('S')) {
+        transform.position_z -= 5.0f * deltaTime;
+    }
+};
 
 // ------------------------------------------------------------ Methods -- == //
 void TestScript::method() {}
