6bfb348365794d5d9ace32e56d218f3e2c8a153f
Adam <adamj500@interia.pl>
2020-05-01 14:29:29 +0200

    Add geometry shader for billboarding

---
+++ PBL_Engine/BillboardGS.hlsl
@@ -0,0 +1,61 @@
+cbuffer CBuf : register(b13){
+    matrix model;
+    matrix viewProj;
+    float4 cameraPos;
+    float textureWidth;
+    float textureHeight;
+};
+
+struct GSOut {
+    float4 pos : SV_Position;
+};
+
+struct VSOut {
+    float4 pos : SV_Position;
+    float4 worldPos : Position;
+    float2 tex : TexCoord;
+};
+
+[maxvertexcount(4)] void GS_Billboard(
+    point VSOut input[1]
+    : SV_POSITION, inout TriangleStream<VSOut> OutputStream) {
+    matrix modelViewProj = mul(model, viewProj);
+    float halfWidth = textureWidth / 2.0f;
+
+    float3 planeNormal = input[0].worldPos - cameraPos;
+    planeNormal.y = 0.0f;
+    planeNormal = normalize(planeNormal);
+
+    float3 upVector = float3(0.0f, 1.0f, 0.0f);
+
+    float3 rightVector = normalize(cross(planeNormal, upVector));
+
+    rightVector = rightVector * halfWidth;
+
+    upVector = float3(0, textureHeight, 0);
+
+    // Create the billboards quad
+    float3 vert[4];
+
+    vert[0] = input[0].worldPos - rightVector;  // Get bottom left vertex
+    vert[1] = input[0].worldPos + rightVector;  // Get bottom right vertex
+    vert[2] =
+        input[0].worldPos - rightVector + upVector;  // Get top left vertex
+    vert[3] =
+        input[0].worldPos + rightVector + upVector;  // Get top right vertex
+
+    // Get billboards texture coordinates
+    float2 texCoord[4];
+    texCoord[0] = float2(0, 1);
+    texCoord[1] = float2(1, 1);
+    texCoord[2] = float2(0, 0);
+    texCoord[3] = float2(1, 0);
+
+    VSOut outputVert;
+    for (int i = 0; i < 4; i++) {
+        outputVert.pos = mul(float4(vert[i], 1.0f), modelViewProj);
+        outputVert.worldPos = float4(vert[i], 0.0f);
+        outputVert.tex = texCoord[i];
+        OutputStream.Append(outputVert);
+    }
+}
