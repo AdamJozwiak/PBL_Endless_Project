84af61622c8104e57cf77a680559e5a7fbed27ce
Tomasz Witczak <TomaszMWitczak@gmail.com>
2020-06-02 00:17:33 +0200

    Add gamma correction to billboards

--- PBL_Engine/BillboardPS.hlsl
+++ PBL_Engine/BillboardPS.hlsl
@@ -10,7 +10,7 @@ struct VSOut {
 float4 main(VSOut input) : SV_Target {
     // Set diffuse color of material
     float4 diffuse;
-    diffuse = Texture.Sample(splr, input.tex);
+    diffuse = pow(Texture.Sample(splr, input.tex), 2.2f);
     clip(diffuse.a - 0.25);
 
     return float4(diffuse);
