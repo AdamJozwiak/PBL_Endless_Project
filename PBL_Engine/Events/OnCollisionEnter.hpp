755da05cd2cf1823786e4c7cb171ef92077f83fd
Adam <adamj500@interia.pl>
2020-06-10 01:36:19 +0200

    Add minimal separating vector to OnCollisionEnter

--- PBL_Engine/Events/OnCollisionEnter.hpp
+++ PBL_Engine/Events/OnCollisionEnter.hpp
@@ -1,6 +1,11 @@
 #pragma once
 
+#include <DirectXMath.h>
+
 #include "ECS/Entity.hpp"
 #include "ECS/Event.hpp"
 
-ECS_EVENT(OnCollisionEnter) { Entity a, b; };
+ECS_EVENT(OnCollisionEnter) {
+    Entity a, b;
+    DirectX::XMFLOAT3 minSeparatingVector;
+};
