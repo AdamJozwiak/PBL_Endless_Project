3991f8f8b3b1e7b39150bf162058758e90507080
Tomasz Witczak <TomaszMWitczak@gmail.com>
2020-06-10 04:55:26 +0200

    Adjust player movement speed

--- PlayerControllerScript/PlayerControllerScript.hpp
+++ PlayerControllerScript/PlayerControllerScript.hpp
@@ -38,7 +38,7 @@ class PLAYERCONTROLLERSCRIPT_API PlayerControllerScript : public Script {
     bool (*isKeyPressed)(int const key);
 
     float movementAcceleration = 40.0f;
-    float runSpeed = 3.0f;
+    float runSpeed = 5.0f;
 
     DirectX::XMFLOAT3 currentVelocity = {0.0f, 0.0f, 0.0f};
     int currentLane = 0;
