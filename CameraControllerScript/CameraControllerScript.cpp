357a126c2c35ab9f03176248082d43576cc7146a
Tomasz Witczak <TomaszMWitczak@gmail.com>
2020-06-21 02:27:51 +0200

    Add smooth camera transitions

--- CameraControllerScript/CameraControllerScript.cpp
+++ CameraControllerScript/CameraControllerScript.cpp
@@ -47,12 +47,34 @@ void CameraControllerScript::setup() {
                        ->findEntityByName("Menu Camera")
                        .at(0)
                        .id;
-    originalTransform = entity.get<Transform>();
-    entity.get<Transform>() = Entity(menuCameraId).get<Transform>();
-    entity.get<Transform>().position.z += 7.5f;
     // registry.system<PropertySystem>()->findEntityByTag("Player").at(0).id;
     offset = entity.get<Transform>().position -
              Entity(playerId).get<Transform>().position;
+    originalTransform = entity.get<Transform>();
+    entity.get<Transform>() = Entity(menuCameraId).get<Transform>();
+    entity.get<Transform>().position.z += 7.5f;
+    middleTransform = entity.get<Transform>();
+    middleTransform.position.x =
+        std::lerp(originalTransform.position.x,
+                  Entity(menuCameraId).get<Transform>().position.x, 0.5f);
+    middleTransform.position.y =
+        std::lerp(originalTransform.position.y,
+                  Entity(menuCameraId).get<Transform>().position.y, 0.5f);
+    middleTransform.position.z =
+        std::lerp(originalTransform.position.z,
+                  Entity(menuCameraId).get<Transform>().position.z, 0.5f);
+    middleTransform.rotation.x =
+        std::lerp(originalTransform.rotation.x,
+                  Entity(menuCameraId).get<Transform>().rotation.x, 0.5f);
+    middleTransform.rotation.y =
+        std::lerp(originalTransform.rotation.y,
+                  Entity(menuCameraId).get<Transform>().rotation.y, 0.5f);
+    middleTransform.rotation.z =
+        std::lerp(originalTransform.rotation.z,
+                  Entity(menuCameraId).get<Transform>().rotation.z, 0.5f);
+    middleTransform.rotation.z =
+        std::lerp(originalTransform.rotation.z,
+                  Entity(menuCameraId).get<Transform>().rotation.z, 0.5f);
 };
 
 void CameraControllerScript::update(float const deltaTime) {
@@ -70,11 +92,62 @@ void CameraControllerScript::update(float const deltaTime) {
         } break;
         case CHANGE_MENU_TYPE_TO_PAUSE: {
         } break;
-        case MENU_TO_GAME_FADE_OUT: {
-        } break;
+        // case MENU_TO_GAME_FADE_OUT: {
+        //     auto& transform = entity.get<Transform>();
+        //     auto easing = &easeOutQuint;
+        //     auto smooth = 0.1f;
+        //     transform.position.x =
+        //         interpolate(easing, transform.position.x,
+        //                     middleTransform.position.x, smooth, deltaTime);
+        //     transform.position.y =
+        //         interpolate(easing, transform.position.y,
+        //                     middleTransform.position.y, smooth, deltaTime);
+        //     transform.position.z =
+        //         interpolate(easing, transform.position.z,
+        //                     middleTransform.position.z, smooth, deltaTime);
+        //     transform.rotation.x =
+        //         interpolate(easing, transform.rotation.x,
+        //                     middleTransform.rotation.x, smooth, deltaTime);
+        //     transform.rotation.y =
+        //         interpolate(easing, transform.rotation.y,
+        //                     middleTransform.rotation.y, smooth, deltaTime);
+        //     transform.rotation.z =
+        //         interpolate(easing, transform.rotation.z,
+        //                     middleTransform.rotation.z, smooth, deltaTime);
+        //     transform.rotation.w =
+        //         interpolate(easing, transform.rotation.w,
+        //                     middleTransform.rotation.w, smooth, deltaTime);
+        // } break;
         case NEW_GAME_SETUP: {
         } break;
+        case MENU_TO_GAME_FADE_OUT:
         case GAME_FADE_IN: {
+            auto& transform = entity.get<Transform>();
+            auto easing = &easeOutQuint;
+            auto smooth = 0.5f;
+            transform.position.x =
+                interpolate(easing, transform.position.x,
+                            originalTransform.position.x, smooth, deltaTime);
+            transform.position.y =
+                interpolate(easing, transform.position.y,
+                            originalTransform.position.y, smooth, deltaTime);
+            transform.position.z =
+                interpolate(easing, transform.position.z,
+                            originalTransform.position.z, smooth, deltaTime);
+            transform.rotation.x =
+                interpolate(easing, transform.rotation.x,
+                            originalTransform.rotation.x, smooth, deltaTime);
+            transform.rotation.y =
+                interpolate(easing, transform.rotation.y,
+                            originalTransform.rotation.y, smooth, deltaTime);
+            transform.rotation.z =
+                interpolate(easing, transform.rotation.z,
+                            originalTransform.rotation.z, smooth, deltaTime);
+            transform.rotation.w =
+                interpolate(easing, transform.rotation.w,
+                            originalTransform.rotation.w, smooth, deltaTime);
+
+            lastPosition = entity.get<Transform>().position;
         } break;
         case GAME: {
             auto const& playerTransform = Entity(playerId).get<Transform>();
@@ -130,7 +203,7 @@ void CameraControllerScript::shake(float const deltaTime) {
                     smoothing, deltaTime),
         interpolate(easeOutQuad, lastPosition.y,
                     playerTransform.position.y + offset.y + shakeOffset.y,
-            smoothing, deltaTime),
+                    smoothing, deltaTime),
         interpolate(easeOutQuad, lastPosition.z,
                     playerTransform.position.z + offset.z + shakeOffset.z,
                     smoothing, deltaTime)};
